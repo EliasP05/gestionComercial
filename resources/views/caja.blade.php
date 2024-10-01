9fce64b0ac91334ff9bea58d48e5b990f7c319a8
Elias05 <eliasalberto0505@gmail.com>
2024-09-30 23:44:43 -0300

    table

--- resources/views/caja.blade.php
+++ resources/views/caja.blade.php
@@ -1,10 +1,34 @@
 <x-layout>
     <div class="flex justify-between items-center">
-        <h1 class="text-4xl font-bold">Algo</h1>  
+        <h1 class="text-4xl font-bold">Caja</h1>  
     </div>
     @session('status')
             <div class="status text-gray-400">
                 {{$value}}
             </div>   
-        @endsession
+    @endsession
+            <table>
+                <thead>
+                    <tr>
+                        <th>cod</th>
+                        <th>Producto</th>
+                        <th>Descripcion</th>
+                        <th>precio unit.</th>
+                        <th>cantidad</th>
+                        <th>subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                            
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
 </x-layout>
